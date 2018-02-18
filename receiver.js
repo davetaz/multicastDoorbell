c69f7b189ce9dea1046bb308adf09800c0408406
davetaz <davetaz@gmail.com>
2018-02-18 10:54:24 +0000

    Yay

--- receiver.js
+++ receiver.js
@@ -2,15 +2,26 @@ var Sound = require('node-aplay');
 var EventEmitter = require('multicast-events').EventEmitter;
 var music = new Sound('/home/pi/multicastDoorbell/doorbell-2.wav');
 
+var playing = false;
+
 var emitter = new EventEmitter({
   name: 'Doorbell',
   foreignOnly: true
 });
 
+music.on('complete', function () {
+	playing = false;	
+});
+
 emitter.on('event-name', function (data) { 
-	music.stop();
-	console.log('Doorbell!'); 
-	setTimeout(function () {
+	console.log('Doorbell!');
+	if (playing) {
+		music.stop();
+		setTimeout(function () {
+    			music.play(); // pause the music after five seconds 
+		}, 100);
+	} else {
 		music.play();
-	}, 100 );
+		playing = true;
+	}
 });
