a5224f962a6797cae1c38f9f9f98f8ea56da91fe
David Tarrant <davetaz@theodi.org>
2020-03-03 08:35:59 +0000

    Updating just sound

--- just-sound.js
+++ just-sound.js
@@ -1,8 +1,15 @@
-var EventEmitter = require('multicast-events').EventEmitter;
+var dgram = require('dgram');
+var s = dgram.createSocket('udp4');
+var MULTICAST_IP = "225.0.0.1";
 
-var emitter = new EventEmitter({
-  name: 'Doorbell',
-  foreignOnly: true
-});
+s.bind(8000);
 
-emitter.emit('event-name', '--> emit from emitter');
+function sendMulticast(s)
+{
+  var b = new Buffer("Multicast Doorbell!");
+  s.send(b, 0, b.length, 8001, MULTICAST_IP, function(err, bytes) {
+    console.log("Sent " + bytes + " bytes");
+  });
+}
+
+sendMulticast(s);
